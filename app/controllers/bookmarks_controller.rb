3e617a9afb51785191e6af41b5edc1542d983fff
Marines Ponce de Leon <marines.poncedeleon@gmail.com>
2022-02-17 13:36:22 -0500

    bookmarks controller done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,14 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
   def new
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to @bookmark, notice: 'Bookmark was successfully created.'
+      redirect_to list_path(@list)
     else
       render :new
     end
@@ -16,12 +17,16 @@ class BookmarksController < ApplicationController
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to root_path
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
